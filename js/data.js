563dedd70edc19b584b81ce70c2a823285e9de7c
Jan Skowronski <crianonim@gmail.com>
2019-07-20 13:36:40 +0100

    Add some more data and screenshots

--- js/data.js
+++ js/data.js
@@ -1,96 +1,104 @@
 console.log("data.js");
 
-const data = [{
-        week: 3,
-        theme: "API",
-        projects: [{
-            title: "Trainpiration",
-            img: "https://camo.githubusercontent.com/2d493d7e712250119309e9c93b4ab091534c0093/68747470733a2f2f692e696d6775722e636f6d2f4348784c4837322e706e67",
-            team: ["Sarah", "Andy", "Reuben", "Jan"],
-            github: "https://github.com/fac-17/week-3-MEIA",
-            description: "Brings you the bussiness inspiration while travelling on the tube."
-        }, {
-            title: "Partyps",
-            img: "",
-            team: ["Leonie", "Gregor", "Martha", "Victor"],
-            github: "https://github.com/fac-17/W3-Partyps",
-            description: "Brings you the bussiness inspiration while travelling on the tube."
-        }, {
-            title: "Kayne or Trump",
-            img: "https://camo.githubusercontent.com/2d493d7e712250119309e9c93b4ab091534c0093/68747470733a2f2f692e696d6775722e636f6d2f4348784c4837322e706e67",
-            team: ["Sarah", "Andy", "Reuben", "Jan"],
-            github: "https://github.com/fac-17/week-3-MEIA",
-            description: "Brings you the bussiness inspiration while travelling on the tube."
-        }, {
-            title: "Crime checker",
-            img: "",
-            team: ["Leonie", "Gregor", "Martha", "Victor"],
-            github: "https://github.com/fac-17/W3-Partyps",
-            description: "Brings you the bussiness inspiration while travelling on the tube."
-        }]
-    },
+const data = {
+    weeks: [{
+            week: 3,
+            theme: "API",
+            projects: [{
+                title: "Trainspiration",
+                img: "assets/w3-trans.png",
+                team: ["Sarah", "Andy", "Reuben", "Jan"],
+                github: "https://github.com/fac-17/week-3-MEIA",
+                description: "Business and travel, trains and inspiration; get a daily dose of wisdom live from trainspiration."
+            }, {
+                title: "Partyps",
+                img: "assets/w3-partyps.png",
+                team: ["Leonie", "Gregor", "Martha", "Victor"],
+                github: "https://github.com/fac-17/W3-Partyps",
+                description: "Have a great party!."
+            }, {
+                title: "Kayne or Trump",
+                img: "assets/w3-kayne-trump.png",
+                team: ["Emmanuel", "Jack", "Colette", "Georgia"],
+                github: "https://github.com/fac-17/Week3-BFNJ",
+                description: "Who said it: Trump or Kanye?"
+            }, {
+                title: "Crime checker",
+                img: "assets/w3-crime.png",
+                team: ["Gigi", "Tony", "Francesca", "Christine"],
+                github: "https://github.com/fac-17/CGKO-Week3",
+                description: "How safe is your area?"
+            }]
+        },
 
-    {
-        week: 1,
-        theme: "Github Workflow",
-        projects: [{
-                title: "Koodi",
-                img: "https://camo.githubusercontent.com/2d493d7e712250119309e9c93b4ab091534c0093/68747470733a2f2f692e696d6775722e636f6d2f4348784c4837322e706e67",
-                team: ["Martha", "Gigi", "Jack", "Jan"],
-                github: "https://github.com/fac-17/week-1-EFGH-project",
-                description: "Landing page for web development agency."
-            }, {
-                title: "ABCD",
-                img: "",
-                team: ["Emmanuel", "Francesca", "Sarah", "Victor"],
-                github: "https://github.com/fac-17/W3-Partyps",
-                description: "Another landing page for web development agency."
-            },
-            {
-                title: "MNOP",
-                img: "",
-                team: ["Leonie", "Andy", "Georgia", "Tony"],
-                github: "https://github.com/fac-17/MNOP",
-                description: "Another one-page portfolio website for our team."
-            }, {
-                title: "ABCD",
-                img: "",
-                team: ["Emmanuel", "Francesca", "Sarah", "Victor"],
-                github: "https://github.com/fac-17/W3-Partyps",
-                description: "Another landing page for web development agency."
-            },
-        ]
-    },
-    {
-        week: 2,
-        theme: "Testing",
-        projects: [{
-                title: "Todo",
-                img: "https://camo.githubusercontent.com/2d493d7e712250119309e9c93b4ab091534c0093/68747470733a2f2f692e696d6775722e636f6d2f4348784c4837322e706e67",
-                team: ["Martha", "Gigi", "Jack", "Jan"],
-                github: "https://github.com/fac-17/week-1-EFGH-project",
-                description: "Landing page for web development agency."
-            }, {
-                title: "AppTitle",
-                img: "",
-                team: ["Emmanuel", "Francesca", "Sarah", "Victor"],
-                github: "https://github.com/fac-17/W3-Partyps",
-                description: "Another landing page for web development agency."
-            },
-            {
-                title: "Another",
-                img: "",
-                team: ["Leonie", "Andy", "Georgia", "Tony"],
-                github: "https://github.com/fac-17/MNOP",
-                description: "Another one-page portfolio website for our team."
-            }, {
-                title: "Great website",
-                img: "",
-                team: ["Emmanuel", "Francesca", "Sarah", "Victor"],
-                github: "https://github.com/fac-17/W3-Partyps",
-                description: "Another landing page for web development agency."
-            },
-        ]
-    },
+        {
+            week: 1,
+            theme: "Github Workflow",
+            projects: [{
+                    title: "Koodi",
+                    img: "https://camo.githubusercontent.com/2d493d7e712250119309e9c93b4ab091534c0093/68747470733a2f2f692e696d6775722e636f6d2f4348784c4837322e706e67",
+                    team: ["Martha", "Gigi", "Jack", "Jan"],
+                    github: "https://github.com/fac-17/week-1-EFGH-project",
+                    description: "Landing page for web development agency."
+                }, {
+                    title: "ABCD",
+                    img: "",
+                    team: ["Emmanuel", "Francesca", "Sarah", "Victor"],
+                    github: "https://github.com/fac-17/W3-Partyps",
+                    description: "Another landing page for web development agency."
+                },
+                {
+                    title: "MNOP",
+                    img: "",
+                    team: ["Leonie", "Andy", "Georgia", "Tony"],
+                    github: "https://github.com/fac-17/MNOP",
+                    description: "Another one-page portfolio website for our team."
+                }, {
+                    title: "ABCD",
+                    img: "",
+                    team: ["Emmanuel", "Francesca", "Sarah", "Victor"],
+                    github: "https://github.com/fac-17/W3-Partyps",
+                    description: "Another landing page for web development agency."
+                },
+            ]
+        },
+        {
+            week: 2,
+            theme: "Testing",
+            projects: [{
+                    title: "Todo",
+                    img: "https://camo.githubusercontent.com/2d493d7e712250119309e9c93b4ab091534c0093/68747470733a2f2f692e696d6775722e636f6d2f4348784c4837322e706e67",
+                    team: ["Martha", "Gigi", "Jack", "Jan"],
+                    github: "https://github.com/fac-17/week-1-EFGH-project",
+                    description: "Landing page for web development agency."
+                }, {
+                    title: "AppTitle",
+                    img: "",
+                    team: ["Emmanuel", "Francesca", "Sarah", "Victor"],
+                    github: "https://github.com/fac-17/W3-Partyps",
+                    description: "Another landing page for web development agency."
+                },
+                {
+                    title: "Another",
+                    img: "",
+                    team: ["Leonie", "Andy", "Georgia", "Tony"],
+                    github: "https://github.com/fac-17/MNOP",
+                    description: "Another one-page portfolio website for our team."
+                }, {
+                    title: "Great website",
+                    img: "",
+                    team: ["Emmanuel", "Francesca", "Sarah", "Victor"],
+                    github: "https://github.com/fac-17/W3-Partyps",
+                    description: "Another landing page for web development agency."
+                },
+            ]
+        },
 
-]
+    ],
+    people:[
+        {name:"Sarah",handle:"sarahyjja",img:"https://avatars0.githubusercontent.com/u/44409053"},
+        {name:"Andy",handle:"andy-mc-donald",img:"https://avatars3.githubusercontent.com/u/41068075"},
+        {name:"Jan",handle:"crianonim",img:"https://avatars2.githubusercontent.com/u/13589494"},
+        {name:"Reuben",handle:"reubengt",img:"https://avatars0.githubusercontent.com/u/42008435"},
+    ]
+}
