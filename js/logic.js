e1d872d903c3c48a718c765cc054f480a30d30a8
Jan Skowronski <crianonim@gmail.com>
2019-07-20 13:50:05 +0100

    Create helper random and shuffle functions

--- js/logic.js
+++ js/logic.js
@@ -2,10 +2,29 @@ console.log("logic.js")
 
 // provide data for the app
 const getData = ()=>{
+    // sort weeks so they start from latest
     data.weeks=data.weeks.sort( (w1,w2)=>w2.week-w1.week );
+    
+    data.weeks=data.weeks.map(week=>{
+        return week;
+    })
+
     return data; 
 }
 
+
+const randomInt=(max)=>{
+    return Math.floor(Math.random()*max);
+}
+
+const shuffleArray=(array)=>{
+    const newArr=[];
+    const arr=array.slice();
+    while (arr.length){
+        newArr.push(arr.splice(randomInt(arr.length),1)[0]);
+    }
+    return newArr;
+}
 const logic={
     getData
 }
