e114328d83aa87d21f3462e416c7cc55e2d1814e
Jan Skowronski <crianonim@gmail.com>
2019-07-20 13:40:48 +0100

    Add week sorting desc

--- js/logic.js
+++ js/logic.js
@@ -2,7 +2,8 @@ console.log("logic.js")
 
 // provide data for the app
 const getData = ()=>{
-    return data;
+    data.weeks=data.weeks.sort( (w1,w2)=>w2.week-w1.week );
+    return data; 
 }
 
 const logic={
