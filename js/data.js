4c2a522d9b27c4cdfe66689e870e6bd86c4fd504
Gigi Minova <minova.gigi@gmail.com>
2019-09-27 01:02:29 +0100

    fixed wrong links in old projects and added images, added placeholders for the missing weeks

--- js/data.js
+++ js/data.js
@@ -3,6 +3,305 @@ console.log("data.js");
 const data = {
   weeks: [
     {
+      week: "14-16",
+      theme: "Tech For Better",
+      projects: [
+        {
+          title: "",
+          img: "",
+          team: ["", "", "", ""],
+          github: "",
+          description: ""
+        },
+        {
+          title: "",
+          img: "",
+          team: ["", "", "", ""],
+          github: "",
+          description: ""
+        },
+        {
+          title: "",
+          img: "",
+          team: ["", "", "", ""],
+          github: "",
+          description: ""
+        },
+        {
+          title: "",
+          img: "",
+          team: ["", "", "", ""],
+          github: "",
+          description: ""
+        }
+      ]
+    },
+    {
+      week: 13,
+      theme: "ReactJS",
+      projects: [
+        {
+          title: "",
+          img: "",
+          team: ["", "", "", ""],
+          github: "",
+          description: ""
+        },
+        {
+          title: "",
+          img: "",
+          team: ["", "", "", ""],
+          github: "",
+          description: ""
+        },
+        {
+          title: "",
+          img: "",
+          team: ["", "", "", ""],
+          github: "",
+          description: ""
+        },
+        {
+          title: "",
+          img: "",
+          team: ["", "", "", ""],
+          github: "",
+          description: ""
+        },
+        {
+          title: "",
+          img: "",
+          team: ["", "", "", ""],
+          github: "",
+          description: ""
+        },
+        {
+          title: "",
+          img: "",
+          team: ["", "", "", ""],
+          github: "",
+          description: ""
+        },
+        {
+          title: "",
+          img: "",
+          team: ["", "", "", ""],
+          github: "",
+          description: ""
+        }
+      ]
+    },
+    {
+      week: "10-12",
+      theme: "Student Projects",
+      projects: [
+        {
+          title: "",
+          img: "",
+          team: ["", "", "", ""],
+          github: "",
+          description: ""
+        },
+        {
+          title: "",
+          img: "",
+          team: ["", "", "", ""],
+          github: "",
+          description: ""
+        },
+        {
+          title: "",
+          img: "",
+          team: ["", "", "", ""],
+          github: "",
+          description: ""
+        },
+        {
+          title: "",
+          img: "",
+          team: ["", "", "", ""],
+          github: "",
+          description: ""
+        }
+      ]
+    },
+    {
+      week: 8,
+      theme: "ExpressJS & HandlebarsJS",
+      projects: [
+        {
+          title: "",
+          img: "",
+          team: ["", "", "", ""],
+          github: "",
+          description: ""
+        },
+        {
+          title: "",
+          img: "",
+          team: ["", "", "", ""],
+          github: "",
+          description: ""
+        },
+        {
+          title: "",
+          img: "",
+          team: ["", "", "", ""],
+          github: "",
+          description: ""
+        },
+        {
+          title: "",
+          img: "",
+          team: ["", "", "", ""],
+          github: "",
+          description: ""
+        }
+      ]
+    },
+    {
+      week: 7,
+      theme: "Authentication",
+      projects: [
+        {
+          title: "",
+          img: "",
+          team: ["", "", "", ""],
+          github: "",
+          description: ""
+        },
+        {
+          title: "",
+          img: "",
+          team: ["", "", "", ""],
+          github: "",
+          description: ""
+        },
+        {
+          title: "",
+          img: "",
+          team: ["", "", "", ""],
+          github: "",
+          description: ""
+        },
+        {
+          title: "",
+          img: "",
+          team: ["", "", "", ""],
+          github: "",
+          description: ""
+        }
+      ]
+    },
+    {
+      week: 6,
+      theme: "PostgreSQL",
+      projects: [
+        {
+          title: "",
+          img: "",
+          team: ["", "", "", ""],
+          github: "",
+          description: ""
+        },
+        {
+          title: "",
+          img: "",
+          team: ["", "", "", ""],
+          github: "",
+          description: ""
+        },
+        {
+          title: "",
+          img: "",
+          team: ["", "", "", ""],
+          github: "",
+          description: ""
+        },
+        {
+          title: "",
+          img: "",
+          team: ["", "", "", ""],
+          github: "",
+          description: ""
+        }
+      ]
+    },
+    {
+      week: 5,
+      theme: "NodeJS 2/2",
+      projects: [
+        {
+          title: "",
+          img: "",
+          team: ["", "", "", ""],
+          github: "",
+          description: ""
+        },
+        {
+          title: "",
+          img: "",
+          team: ["", "", "", ""],
+          github: "",
+          description: ""
+        },
+        {
+          title: "",
+          img: "",
+          team: ["", "", "", ""],
+          github: "",
+          description: ""
+        },
+        {
+          title: "",
+          img: "",
+          team: ["", "", "", ""],
+          github: "",
+          description: ""
+        }
+      ]
+    },
+    {
+      week: 4,
+      theme: "NodeJS 1/2",
+      projects: [
+        {
+          title: "Postcode Autocomplete",
+          img: "assets/w4-postcode.png",
+          team: ["Tony", "Gigi", "Francesca", "Christine"],
+          github: "https://github.com/fac-17/cgko-week-4",
+          description:
+            "Our aim was to build a simple postcode autocomplete app using Node.JS, HTML and CSS."
+        },
+        {
+          title: "Wikipedia Country Lookup",
+          img:
+            "https://camo.githubusercontent.com/ff853cddc8de71effdd50de4e2799fce90a6f14a/68747470733a2f2f692e696d6775722e636f6d2f3263367a4b49322e706e67",
+          team: ["Jan", "Reuben", "Sarah", "Andy"],
+          github: "https://github.com/fac-17/week4_MEIA_Autocomplete",
+          description:
+            "A simple autocomplete widget that accepts a search input and shows an auto-updating list of completed countries."
+        },
+        {
+          title: "Search Engine for Films",
+          img:
+            "https://github.com/fac-17/Puteam-Week4/blob/master/public/img/camera.png?raw=true",
+          team: ["Gregor", "Victor", "Martha", "Leonie"],
+          github: "https://github.com/fac-17/Puteam-Week4",
+          description: "A search engine for movie synopses."
+        },
+        {
+          title: "You speak Newspeak",
+          img:
+            "https://camo.githubusercontent.com/d3273e2ebbdc6475cccd4090457c4902185652e9/68747470733a2f2f736f757263656d616b696e672e636f6d2f66696c65732f736d2f696d616765732f737061676574742e6a7067",
+          team: ["Jack", "Georgia", "Emmanuel", "Colette"],
+          github: "https://github.com/emaggy/you-speak-newspeak",
+          description:
+            "the repo name references Newspeak from 1984, but we changed the project to use Nadsat from A Clockwork Orange"
+        }
+      ]
+    },
+    {
       week: 3,
       theme: "API",
       projects: [
@@ -37,7 +336,44 @@ const data = {
         }
       ]
     },
-
+    {
+      week: 2,
+      theme: "Testing",
+      projects: [
+        {
+          title: "To-Do",
+          img:
+            "https://camo.githubusercontent.com/8f79873f676d1e6bfb98215f920937518b8ac167/68747470733a2f2f692e696d6775722e636f6d2f305859627157722e6a7067",
+          team: ["Martha", "Gigi", "Jack", "Jan"],
+          github: "https://github.com/fac-17/week-2-EFGH-to-do-app",
+          description: "Use Test driven development(TDD) to build a To-do app"
+        },
+        {
+          title: "To-Do",
+          img:
+            "https://camo.githubusercontent.com/10ed32595e7bbec97b417a1ea99da2c0fdba8ed9/68747470733a2f2f692e696d6775722e636f6d2f554d666d3576702e6a7067",
+          team: ["Emmanuel", "Francesca", "Sarah", "Victor"],
+          github: "https://github.com/fac-17/ABCD-week2-project",
+          description: "Another landing page for web development agency."
+        },
+        {
+          title: "To-Do",
+          img:
+            "https://camo.githubusercontent.com/6b14d068654d36e01fb0f241f96ed8dd606ba435/68747470733a2f2f692e696d6775722e636f6d2f626652367a4e582e706e67",
+          team: ["Leonie", "Andy", "Georgia", "Tony"],
+          github: "https://github.com/fac-17/MNOP-week2-to-do-app",
+          description: "Another to-do project"
+        },
+        {
+          title: "To-Do",
+          img: "assets/w3-IJKL-todo.png",
+          team: ["Christine", "Colette", "Reuben", "Gregor"],
+          github: "https://github.com/fac-17/IJKL-week2-project",
+          description:
+            "Group IJKL's to-do list app - like the others, but with less functionality!"
+        }
+      ]
+    },
     {
       week: 1,
       theme: "Github Workflow",
@@ -45,21 +381,23 @@ const data = {
         {
           title: "Koodi",
           img:
-            "https://camo.githubusercontent.com/2d493d7e712250119309e9c93b4ab091534c0093/68747470733a2f2f692e696d6775722e636f6d2f4348784c4837322e706e67",
+            "https://camo.githubusercontent.com/4e07e43979ee471293db191d15a43b055571316f/68747470733a2f2f692e696d6775722e636f6d2f30534445706b362e6a7067",
           team: ["Martha", "Gigi", "Jack", "Jan"],
           github: "https://github.com/fac-17/week-1-EFGH-project",
           description: "Landing page for web development agency."
         },
         {
           title: "ABCD",
-          img: "",
+          img:
+            "https://github.com/fac-17/ABCD-week1/blob/master/images/jacksons.png?raw=true",
           team: ["Emmanuel", "Francesca", "Sarah", "Victor"],
-          github: "https://github.com/fac-17/W3-Partyps",
+          github: "https://github.com/fac-17/ABCD-week1",
           description: "Another landing page for web development agency."
         },
         {
           title: "MNOP",
-          img: "",
+          img:
+            "https://github.com/fac-17/MNOP/blob/master/images/BulbImage.jpg?raw=true",
           team: ["Leonie", "Andy", "Georgia", "Tony"],
           github: "https://github.com/fac-17/MNOP",
           description: "Another one-page portfolio website for our team."
@@ -70,42 +408,6 @@ const data = {
           team: ["Christine", "Colette", "Reuben", "Gregor"],
           github: "https://github.com/fac-17/IJKL-Week1-Project",
           description: 'Portfolio website for "Serious Web Development".'
-        }
-      ]
-    },
-    {
-      week: 2,
-      theme: "Testing",
-      projects: [
-        {
-          title: "Todo",
-          img:
-            "https://camo.githubusercontent.com/2d493d7e712250119309e9c93b4ab091534c0093/68747470733a2f2f692e696d6775722e636f6d2f4348784c4837322e706e67",
-          team: ["Martha", "Gigi", "Jack", "Jan"],
-          github: "https://github.com/fac-17/week-1-EFGH-project",
-          description: "Landing page for web development agency."
-        },
-        {
-          title: "AppTitle",
-          img: "",
-          team: ["Emmanuel", "Francesca", "Sarah", "Victor"],
-          github: "https://github.com/fac-17/W3-Partyps",
-          description: "Another landing page for web development agency."
-        },
-        {
-          title: "Another",
-          img: "",
-          team: ["Leonie", "Andy", "Georgia", "Tony"],
-          github: "https://github.com/fac-17/MNOP",
-          description: "Another one-page portfolio website for our team."
-        },
-        {
-          title: "To-Do List App",
-          img: "assets/w3-IJKL-todo.png",
-          team: ["Christine", "Colette", "Reuben", "Gregor"],
-          github: "https://github.com/fac-17/IJKL-week2-project",
-          description:
-            "Group IJKL's to-do list app - like the others, but with less functionality!"
         }
       ]
     }
