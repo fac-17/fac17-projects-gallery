c619ec6d26c41f79ab7a47aa43d785f024e0cb87
Gigi Minova <minova.gigi@gmail.com>
2019-09-27 01:48:44 +0100

    added data for student projects week 10-12"

--- js/data.js
+++ js/data.js
@@ -96,32 +96,37 @@ const data = {
       theme: "Student Projects",
       projects: [
         {
-          title: "",
-          img: "",
-          team: ["", "", "", ""],
-          github: "",
-          description: ""
+          title: "Food Miles",
+          img:"https://camo.githubusercontent.com/cddd190bc6c9499325016f61844e7105edafdb4b/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f7961786349484f354f675034592f67697068792e676966",
+          team: ["Jack", "Jan", "Gigi", "Andy"],
+          github: "https://github.com/fac-17/FFFF",
+          description:
+            "As a user, I need an easy way to find out how local my shopping list is by comparing supermarkets and where they source their food from."
         },
         {
-          title: "",
-          img: "",
-          team: ["", "", "", ""],
-          github: "",
-          description: ""
+          title: "NomNom",
+          img:
+            "https://camo.githubusercontent.com/3279d81da6412c5ba5a1ea261bb78fb564ac4eff/68747470733a2f2f692e696d6775722e636f6d2f536156343179522e706e67",
+          team: ["Francesca", "Tony", "Martha", "Victor"],
+          github: "https://github.com/fac-17/NomNom",
+          description:
+            "Build a planning meal app that suggest a week-long meal plans with common ingredients and provides healthy options in order to reduce food waste."
         },
         {
-          title: "",
-          img: "",
-          team: ["", "", "", ""],
-          github: "",
-          description: ""
+          title: "Meet me in the middle",
+          img:
+            "data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4NCjwhLS0gR2VuZXJhdG9yOiBB%0D%0AZG9iZSBJbGx1c3RyYXRvciAxNi4wLjAsIFNWRyBFeHBvcnQgUGx1Zy1JbiAuIFNWRyBWZXJzaW9u%0D%0AOiA2LjAwIEJ1aWxkIDApICAtLT4NCjwhRE9DVFlQRSBzdmcgUFVCTElDICItLy9XM0MvL0RURCBT%0D%0AVkcgMS4xLy9FTiIgImh0dHA6Ly93d3cudzMub3JnL0dyYXBoaWNzL1NWRy8xLjEvRFREL3N2ZzEx%0D%0ALmR0ZCI+DQo8c3ZnIHZlcnNpb249IjEuMSIgaWQ9IkNhbHF1ZV8xIiB4bWxucz0iaHR0cDovL3d3%0D%0Ady53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hs%0D%0AaW5rIiB4PSIwcHgiIHk9IjBweCINCgkgd2lkdGg9IjExOC42cHgiIGhlaWdodD0iMTQxLjQxN3B4%0D%0AIiB2aWV3Qm94PSIwIDAgMTE4LjYgMTQxLjQxNyIgZW5hYmxlLWJhY2tncm91bmQ9Im5ldyAwIDAg%0D%0AMTE4LjYgMTQxLjQxNyIgeG1sOnNwYWNlPSJwcmVzZXJ2ZSI+DQo8Zz4NCgk8cGF0aCBmaWxsPSIj%0D%0ARkZGRkZGIiBzdHJva2U9IiMwMDAwMDAiIHN0cm9rZS13aWR0aD0iNCIgc3Ryb2tlLW1pdGVybGlt%0D%0AaXQ9IjEwIiBkPSJNNTkuMywyNC44MTdjLTMxLjY0NiwwLTU3LjMsMjUuNjU0LTU3LjMsNTcuMw0K%0D%0ACQljMCwzMS42NDYsMjUuNjU0LDU3LjMsNTcuMyw1Ny4zczU3LjI5OS0yNS42NTMsNTcuMjk5LTU3%0D%0ALjNDMTE2LjYsNTAuNDcyLDkwLjk0NiwyNC44MTcsNTkuMywyNC44MTd6IE01OS4zLDEyNi43NTEN%0D%0ACgkJYy0yNC42NSwwLTQ0LjYzMy0xOS45ODMtNDQuNjMzLTQ0LjYzNFMzNC42NSwzNy40ODQsNTku%0D%0AMywzNy40ODRjMjQuNjUxLDAsNDQuNjMzLDE5Ljk4Miw0NC42MzMsNDQuNjMzDQoJCVM4My45NTEs%0D%0AMTI2Ljc1MSw1OS4zLDEyNi43NTF6Ii8+DQoJPGNpcmNsZSBmaWxsPSJub25lIiBzdHJva2U9IiMw%0D%0AMDAwMDAiIHN0cm9rZS13aWR0aD0iNCIgc3Ryb2tlLW1pdGVybGltaXQ9IjEwIiBjeD0iNTkuMyIg%0D%0AY3k9IjEyLjc0MyIgcj0iMTAuNzQ0Ii8+DQoJPHBhdGggZD0iTTYzLjA5NCwyNC44MTdoLTcuNTg3%0D%0Adi05Ljg1NGMwLTEuMDcsMC44NjgtMS45MzgsMS45MzgtMS45MzhoMy43MTJjMS4wNywwLDEuOTM4%0D%0ALDAuODY3LDEuOTM4LDEuOTM4VjI0LjgxN3oiLz4NCgk8bGluZSBzdHJva2U9IiMwMDAwMDAiIHN0%0D%0Acm9rZS13aWR0aD0iNCIgc3Ryb2tlLW1pdGVybGltaXQ9IjEwIiB4MT0iNTkuMyIgeTE9IjM3LjQ4%0D%0ANCIgeDI9IjU5LjMiIHkyPSI0Ni44MTciLz4NCgk8bGluZSBzdHJva2U9IiMwMDAwMDAiIHN0cm9r%0D%0AZS13aWR0aD0iNCIgc3Ryb2tlLW1pdGVybGltaXQ9IjEwIiB4MT0iMjcuNzQiIHkxPSI1MC41NTci%0D%0AIHgyPSIzNC4zMzkiIHkyPSI1Ny4xNTYiLz4NCgk8bGluZSBzdHJva2U9IiMwMDAwMDAiIHN0cm9r%0D%0AZS13aWR0aD0iNCIgc3Ryb2tlLW1pdGVybGltaXQ9IjEwIiB4MT0iMTQuNjY3IiB5MT0iODIuMTE3%0D%0AIiB4Mj0iMjQiIHkyPSI4Mi4xMTciLz4NCgk8bGluZSBmaWxsPSJub25lIiBzdHJva2U9IiMwMDAw%0D%0AMDAiIHN0cm9rZS13aWR0aD0iNCIgc3Ryb2tlLW1pdGVybGltaXQ9IjEwIiB4MT0iMjcuNzQiIHkx%0D%0APSIxMTMuNjc4IiB4Mj0iMzQuMzM5IiB5Mj0iMTA3LjA3OCIvPg0KCTxsaW5lIHN0cm9rZT0iIzAw%0D%0AMDAwMCIgc3Ryb2tlLXdpZHRoPSI0IiBzdHJva2UtbWl0ZXJsaW1pdD0iMTAiIHgxPSI1OS4zIiB5%0D%0AMT0iMTI2Ljc1MSIgeDI9IjU5LjMiIHkyPSIxMTcuNDE3Ii8+DQoJPGxpbmUgc3Ryb2tlPSIjMDAw%0D%0AMDAwIiBzdHJva2Utd2lkdGg9IjQiIHN0cm9rZS1taXRlcmxpbWl0PSIxMCIgeDE9IjkwLjg2MSIg%0D%0AeTE9IjExMy42NzgiIHgyPSI4NC4yNjIiIHkyPSIxMDcuMDc4Ii8+DQoJPGxpbmUgc3Ryb2tlPSIj%0D%0AMDAwMDAwIiBzdHJva2Utd2lkdGg9IjQiIHN0cm9rZS1taXRlcmxpbWl0PSIxMCIgeDE9IjEwMy45%0D%0AMzQiIHkxPSI4Mi4xMTciIHgyPSI5NC42IiB5Mj0iODIuMTE3Ii8+DQoJPGxpbmUgZmlsbD0ibm9u%0D%0AZSIgc3Ryb2tlPSIjMDAwMDAwIiBzdHJva2Utd2lkdGg9IjQiIHN0cm9rZS1taXRlcmxpbWl0PSIx%0D%0AMCIgeDE9IjkwLjg2MSIgeTE9IjUwLjU1NyIgeDI9Ijg0LjI2MiIgeTI9IjU3LjE1NiIvPg0KCTxn%0D%0APg0KCQk8cG9seWdvbiBwb2ludHM9IjU5LjMsNTIuMTIxIDUxLjc0LDgyLjExNyA1OS4zLDExMi4x%0D%0AMTMgNjYuODYsODIuMTE3IAkJIi8+DQoJCTxwb2x5Z29uIHBvaW50cz0iODAuNTExLDYwLjkwNyA1%0D%0AMy45NTUsNzYuNzcxIDM4LjA5LDEwMy4zMjcgNjQuNjQ2LDg3LjQ2MyAJCSIvPg0KCQk8cG9seWdv%0D%0AbiBwb2ludHM9Ijg5LjI5Niw4Mi4xMTcgNTkuMyw3NC41NTggMjkuMzA1LDgyLjExNyA1OS4zLDg5%0D%0ALjY3OCAJCSIvPg0KCQk8cG9seWdvbiBwb2ludHM9IjgwLjUxMSwxMDMuMzI3IDY0LjY0Niw3Ni43%0D%0ANzEgMzguMDksNjAuOTA3IDUzLjk1NSw4Ny40NjMgCQkiLz4NCgk8L2c+DQoJPHBvbHlnb24gZmls%0D%0AbD0iI0ZGRUU0QyIgc3Ryb2tlPSIjMDAwMDAwIiBzdHJva2Utd2lkdGg9IjQiIHN0cm9rZS1taXRl%0D%0AcmxpbWl0PSIxMCIgcG9pbnRzPSI4My44MDEsNTcuNjE3IDUxLjc0LDc0LjU1OCANCgkJMzQuOCwx%0D%0AMDYuNjE3IDY2Ljg2LDg5LjY3OCAJIi8+DQoJPGNpcmNsZSBmaWxsPSIjRkZFRTRDIiBzdHJva2U9%0D%0AIiMwMDAwMDAiIHN0cm9rZS13aWR0aD0iNCIgc3Ryb2tlLW1pdGVybGltaXQ9IjEwIiBjeD0iNTku%0D%0AMyIgY3k9IjgyLjExNyIgcj0iOS4wMzEiLz4NCjwvZz4NCjwvc3ZnPg0K",
+          team: ["Georgia", "Reuben", "Sarah", "Leonie"],
+          github: "https://github.com/fac-17/Where-Should-We-Meet",
+          description:
+            "Hi! wave Welcome to Meet Me In The Middle - the web app that takes your round_pushpin location, takes your friend's round_pushpinlocation, and finds you a venue classical_building to meet at in the middle!"
         },
         {
-          title: "",
-          img: "",
-          team: ["", "", "", ""],
-          github: "",
-          description: ""
+          title: "PlantbaseMe",
+          img: "https://github.com/fac-17/Plantbase-Me/blob/master/public/assets/Figma.png?raw=true",
+          team: ["Sam", "Christine", "Gregor", "Emmanuel", "Colette"],
+          github: "https://github.com/fac-17/Plantbase-Me",
+          description: "Our app idea lets a user search for a non-vegan dish that they might be craving, and shows them a good alternative."
         }
       ]
     },
@@ -131,24 +136,29 @@ const data = {
       projects: [
         {
           title: "Amazing, Adorable Animal Adoption Agency",
-          img: "https://camo.githubusercontent.com/e8c6b092cd530156eaf0d6a6819a62db52d5d2ec/68747470733a2f2f692e696d6775722e636f6d2f464c6a53674f762e706e67",
+          img:
+            "https://camo.githubusercontent.com/e8c6b092cd530156eaf0d6a6819a62db52d5d2ec/68747470733a2f2f692e696d6775722e636f6d2f464c6a53674f762e706e67",
           team: ["Francesca", "Jack", "Andy", "Gregor"],
           github: "https://github.com/fac-17/CFLM",
           description: "Build a server-rendered full-stack app"
         },
         {
           title: "Pearls of Wisdom",
-          img: "https://camo.githubusercontent.com/86d6cd5523b35781bb8440f801d6f56b8a9bc15e/68747470733a2f2f692e696d6775722e636f6d2f785a336d4852732e706e67",
+          img:
+            "https://camo.githubusercontent.com/86d6cd5523b35781bb8440f801d6f56b8a9bc15e/68747470733a2f2f692e696d6775722e636f6d2f785a336d4852732e706e67",
           team: ["Reuben", "Gigi", "Leonie", "Emmanuel"],
           github: "https://github.com/fac-17/BGIP",
-          description: "Pearls of Wisdom is a quote taking app that allows users to log quotes and add them to a category"
+          description:
+            "Pearls of Wisdom is a quote taking app that allows users to log quotes and add them to a category"
         },
         {
           title: "Tarot Lady",
-          img: "https://camo.githubusercontent.com/2bb706c3e2811be5650186cd5094cf00ae7cef25/68747470733a2f2f692e696d6775722e636f6d2f344c335a6f6b642e706e67",
+          img:
+            "https://camo.githubusercontent.com/2bb706c3e2811be5650186cd5094cf00ae7cef25/68747470733a2f2f692e696d6775722e636f6d2f344c335a6f6b642e706e67",
           team: ["Georgia", "Jan", "Christine", "Victor"],
           github: "https://github.com/fac-17/DENK",
-          description: "User visits a page and chooses his card, clicking on it. User is then transferred to a tarot reading side and can see the card and the description."
+          description:
+            "User visits a page and chooses his card, clicking on it. User is then transferred to a tarot reading side and can see the card and the description."
         },
         {
           title: "Fun FACts",
@@ -165,7 +175,8 @@ const data = {
       projects: [
         {
           title: "BugBears continued",
-          img: "https://github.com/fac-17/Week7-Bugbears-continued/blob/master/public/bugbear-bear.png?raw=true",
+          img:
+            "https://github.com/fac-17/Week7-Bugbears-continued/blob/master/public/bugbear-bear.png?raw=true",
           team: ["Martha", "Emmanuel", "Christine", "Andy"],
           github: "https://github.com/fac-17/Week7-Bugbears-continued",
           description: "An app to post about things that bug you"
@@ -179,17 +190,20 @@ const data = {
         },
         {
           title: "SaVaGe Creator",
-          img: "https://camo.githubusercontent.com/5c55e29e0749fdd0fb3ac6625e7a31ce14abc73e/68747470733a2f2f692e696d6775722e636f6d2f765167663370482e6a7067",
+          img:
+            "https://camo.githubusercontent.com/5c55e29e0749fdd0fb3ac6625e7a31ce14abc73e/68747470733a2f2f692e696d6775722e636f6d2f765167663370482e6a7067",
           team: ["Colette", "Leonie", "Jan", "Francesca"],
           github: "https://github.com/fac-17/SaVaGe",
           description: "SaVaGe Artwork Creator by Bumpy and The Midwives"
         },
         {
-          title: "Final FACtasy"  ,
-          img: "https://camo.githubusercontent.com/51e55d26e251f2ac5c79f8751d34272256a0c642/68747470733a2f2f692e696d6775722e636f6d2f39574e597557562e6a7067",
+          title: "Final FACtasy",
+          img:
+            "https://camo.githubusercontent.com/51e55d26e251f2ac5c79f8751d34272256a0c642/68747470733a2f2f692e696d6775722e636f6d2f39574e597557562e6a7067",
           team: ["Reuben", "Jack", "Victor", "Tony"],
           github: "https://github.com/fac-17/Week6-DFIO",
-          description: "A fantasy game where you purchase items to battle other users!"
+          description:
+            "A fantasy game where you purchase items to battle other users!"
         }
       ]
     },
@@ -198,15 +212,18 @@ const data = {
       theme: "PostgreSQL",
       projects: [
         {
-          title: "Final FACtasy"  ,
-          img: "https://camo.githubusercontent.com/51e55d26e251f2ac5c79f8751d34272256a0c642/68747470733a2f2f692e696d6775722e636f6d2f39574e597557562e6a7067",
+          title: "Final FACtasy",
+          img:
+            "https://camo.githubusercontent.com/51e55d26e251f2ac5c79f8751d34272256a0c642/68747470733a2f2f692e696d6775722e636f6d2f39574e597557562e6a7067",
           team: ["Reuben", "Jack", "Victor", "Tony"],
           github: "https://github.com/fac-17/Week6-DFIO",
-          description: "A fantasy game where you purchase items to battle other users!"
+          description:
+            "A fantasy game where you purchase items to battle other users!"
         },
         {
           title: "BugBears",
-          img: "https://camo.githubusercontent.com/87b68c36b5d9f4e031331dda13d9f851be050889/68747470733a2f2f692e696d6775722e636f6d2f6b354c4c374b6d2e706e67",
+          img:
+            "https://camo.githubusercontent.com/87b68c36b5d9f4e031331dda13d9f851be050889/68747470733a2f2f692e696d6775722e636f6d2f6b354c4c374b6d2e706e67",
           team: ["Martha", "Emmanuel", "Christine", "Andy"],
           github: "https://github.com/fac-17/Week-6-BHKM-",
           description: "An app to post about things that bug you"
@@ -216,11 +233,13 @@ const data = {
           img: "https://i.imgur.com/ptPwdaX.jpg",
           team: ["Gigi", "Georgia", "Sarah", "Gregor"],
           github: "https://github.com/fac-17/week-6-glan",
-          description: "A brave warrior enters their name and some personality traits and is returned the name of the glan they belong to. "
+          description:
+            "A brave warrior enters their name and some personality traits and is returned the name of the glan they belong to. "
         },
         {
           title: "SaVaGe Creator",
-          img: "https://camo.githubusercontent.com/5c55e29e0749fdd0fb3ac6625e7a31ce14abc73e/68747470733a2f2f692e696d6775722e636f6d2f765167663370482e6a7067",
+          img:
+            "https://camo.githubusercontent.com/5c55e29e0749fdd0fb3ac6625e7a31ce14abc73e/68747470733a2f2f692e696d6775722e636f6d2f765167663370482e6a7067",
           team: ["Colette", "Leonie", "Jan", "Francesca"],
           github: "https://github.com/fac-17/SaVaGe",
           description: "SaVaGe Artwork Creator by Bumpy and The Midwives"
@@ -233,28 +252,34 @@ const data = {
       projects: [
         {
           title: "Country News App",
-          img: "https://camo.githubusercontent.com/b4053123db29bd81f925dabcfe7ee1a0f93522d1/68747470733a2f2f692e696d6775722e636f6d2f7875646745755a2e6a7067",
+          img:
+            "https://camo.githubusercontent.com/b4053123db29bd81f925dabcfe7ee1a0f93522d1/68747470733a2f2f692e696d6775722e636f6d2f7875646745755a2e6a7067",
           team: ["Sarah", "Leonie", "Jack", "Christine"],
           github: "https://github.com/fac-17/PFAK-week5",
           description: "TOP NEWS WORLDWIDE"
         },
         {
           title: "Stock prices of commodities",
-          img: "https://github.com/fac-17/week5-chin-project/blob/master/public/images/tinified/Cattle-Image.jpg?raw=true",
+          img:
+            "https://github.com/fac-17/week5-chin-project/blob/master/public/images/tinified/Cattle-Image.jpg?raw=true",
           team: ["Reuben", "Georgia", "Martha", "Francesca"],
           github: "https://github.com/fac-17/week5-chin-project",
-          description: "Create a web app that includes some form of user input and provides users with content from an API that is regularly updated"
+          description:
+            "Create a web app that includes some form of user input and provides users with content from an API that is regularly updated"
         },
         {
           title: "Project Fear",
-          img: "https://camo.githubusercontent.com/ea58324e343e408d54b4efd7be23a3015907a2f9/68747470733a2f2f692e696d6775722e636f6d2f4e4f746b4761752e706e67",
+          img:
+            "https://camo.githubusercontent.com/ea58324e343e408d54b4efd7be23a3015907a2f9/68747470733a2f2f692e696d6775722e636f6d2f4e4f746b4761752e706e67",
           team: ["Gigi", "Andy", "Victor", "Colette"],
           github: "https://github.com/fac-17/week-5-dgjm",
-          description: "Display live information on the Pound vs other currencies"
+          description:
+            "Display live information on the Pound vs other currencies"
         },
         {
           title: "BELO NEWS",
-          img: "https://camo.githubusercontent.com/96a41a1be0cd0374fc30379451844a008e95e019/68747470733a2f2f692e696d6775722e636f6d2f4b637a496c73332e6a7067",
+          img:
+            "https://camo.githubusercontent.com/96a41a1be0cd0374fc30379451844a008e95e019/68747470733a2f2f692e696d6775722e636f6d2f4b637a496c73332e6a7067",
           team: ["Tony", "Jan", "Emmanuel", "Gregor"],
           github: "https://github.com/fac-17/BELO",
           description: "Simple news app."
