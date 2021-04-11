6c68ae46527c5899a4d7c6b6da06faa1d0a3c9dc
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2021-04-11 18:44:22 +0100

    Add projects from weeks 2-4 including mini-projects

--- js/data.js
+++ js/data.js
@@ -38,7 +38,186 @@ const data = {
           description: 'Landing page for web development agency.".'
         }
       ]
-    }
+    },
+    {
+      week: 2,
+      theme: "HTTP",
+      projects: [
+        {
+          title: "Dating App",
+          img:
+            "https://fac21.github.io/week2-mnns/images/heart.svg",
+          team: ["Nafisa", "Neville", "Saki", "Maryam"],
+          github: "https://github.com/fac21/week2-mnns",
+          description: "An app that queries at least 2 APIs"
+        },
+        {
+          title: "Cheer Me Up!",
+          img:
+            "https://user-images.githubusercontent.com/57327617/111757799-a08b8400-8893-11eb-8833-da511201c179.png",
+          team: ["Rosie", "Evgeny", "Pineapple", "Chun"],
+          github: "https://github.com/fac21/week2-http-project-ECRC",
+          description: "An app that queries at least 2 APIs"
+        },
+        {
+          title: "Astrologia de cada dia",
+          img:
+            "https://camo.githubusercontent.com/b4942420244f4f486bddf7528e21568763663e2cc62d4e1c657bd7e84bcd7a56/68747470733a2f2f692e696d6775722e636f6d2f72376d655748562e706e67",
+          team: ["Craig", "Amy", "Safia", "Michael"],
+          github: "https://github.com/fac21/week-2-astrologia-de-cada-dia",
+          description: "An app that queries at least 2 APIs"
+        },
+        {
+          title: "Check your Company Name",
+          img: "https://raw.githubusercontent.com/fac21/week2--Http-Project--AJMS/main/images/loading.png",
+          team: ["Jo", "Sevda", "Antonio", "Mariya"],
+          github: "https://github.com/fac21/week2--Http-Project--AJMS",
+          description: 'An app that queries at least 2 APIs.'
+        }
+      ]
+    },
+    {
+      week: 3,
+      theme: "Testing",
+      projects: [
+        {
+          title: "Catalog",
+          img:
+            "https://raw.githubusercontent.com/fac21/week3-catalog-EM2C/main/images/project.png",
+          team: ["Mariya", "Craig", "Evgeny", "Maryam"],
+          github: "https://github.com/fac21/week3-catalog-EM2C",
+          description: "A to-do list"
+        },
+        {
+          title: "AANR",
+          img:
+            "assets/week3-AANR.png",
+          team: ["Rosie", "Amy", "Nafisa", "Antonio"],
+          github: "https://github.com/fac21/WEEK3-AANR",
+          description: "A to-do list"
+        },
+        {
+          title: "To Do List",
+          img:
+            "https://user-images.githubusercontent.com/23028288/112509369-4fdec400-8d88-11eb-8739-4eeee857f650.png",
+          team: ["Jo", "Chun", "Saki", "Michael"],
+          github: "https://github.com/fac21/week3-CJMS-todo-list",
+          description: "A to-do list"
+        },
+        {
+          title: "CNSS",
+          img: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/sea-of-yellow-and-orange-daffodils-royalty-free-image-135169065-1552990435.jpg?crop=0.66667xw:1xh;center,top&resize=768:*",
+          team: ["Pineapple", "Sevda", "Neville", "Safia"],
+          github: "https://github.com/fac21/Week-3-Testing-CNSS",
+          description: 'A to-do list'
+        }
+      ]
+    },
+    {
+      week: 4,
+      theme: "Mini-Projects",
+      projects: [
+        {
+          title: "Poke-Whack",
+          img:
+            "https://raw.githubusercontent.com/fac21/Poke-whack-AR/50a041db677dc91fb2b31d98178f4d7017685ad5/pokeball.svg",
+          team: ["Amy", "Rosie"],
+          github: "https://github.com/fac21/Poke-whack-AR",
+          description: "A whack-a-mole style game"
+        },
+        {
+          title: "Get Inspired!",
+          img:
+            "assets/mini-project-AN.png",
+          team: ["Nafisa", "Antonio"],
+          github: "https://github.com/fac21/an-project",
+          description: "A text-to-speech quote reader"
+        },
+        {
+          title: "Guess Who",
+          img:
+            "assets/mini-project-2M.png",
+          team: ["Mariya", "Maryam"],
+          github: "https://github.com/fac21/Mini-project-guesswho-2M",
+          description: "Inspired by Wes Bos's Javascript 30 challenge"
+        },
+        {
+          title: "Guac-A-Mole",
+          img: "https://fac21.github.io/Guac-A-Mole/bowl.png",
+          team: ["Craig", "Safia"],
+          github: "https://github.com/fac21/Guac-A-Mole",
+          description: "A whack-a-mole style game"
+        },
+        {
+          title: "Speak To Me",
+          img:
+            "assets/mini-project-EC.png",
+          team: ["Pineapple", "Evgeny"],
+          github: "https://github.com/fac21/Mini-Project-Chisha-Evgeny",
+          description: "Speech-to-text recognition"
+        },
+        {
+          title: "The Mindfulness Colouring Book",
+          img:
+            "https://user-images.githubusercontent.com/31373245/114215987-36c83c80-995e-11eb-91fa-82f557c30063.png",
+          team: ["Jo", "Saki"],
+          github: "https://github.com/fac21/miniprojectcanvas-sakijo",
+          description: "A HTML5 canvas project"
+        },
+        {
+          title: "Madceez",
+          img:
+            "assets/mini-project-CM.png",
+          team: ["Michael", "Chun"],
+          github: "https://github.com/fac21/pair-project",
+          description: "An interactive synth"
+        },
+        {
+          title: "Whack-A-Mole",
+          img: "https://fac21.github.io/Week3-Mini-Project-NS/mole.svg",
+          team: ["Neville", "Sevda"],
+          github: "https://github.com/fac21/Week3-Mini-Project-NS",
+          description: "A whack-a-mole style game"
+        }
+      ]
+    },
+    // {
+    //   week: 4,
+    //   theme: "Server",
+    //   projects: [
+    //     {
+    //       title: "?",
+    //       img:
+    //         "#",
+    //       team: ["Mariya", "Craig", "Evgeny", "Maryam"],
+    //       github: "#",
+    //       description: "?"
+    //     },
+    //     {
+    //       title: "?",
+    //       img:
+    //         "#",
+    //       team: ["Rosie", "Amy", "Nafisa", "Antonio"],
+    //       github: "#",
+    //       description: "?"
+    //     },
+    //     {
+    //       title: "?",
+    //       img:
+    //         "#",
+    //       team: ["Jo", "Chun", "Saki", "Michael"],
+    //       github: "#",
+    //       description: "?"
+    //     },
+    //     {
+    //       title: "?",
+    //       img: "#",
+    //       team: ["Pineapple", "Sevda", "Neville", "Safia"],
+    //       github: "#",
+    //       description: "?"
+    //     }
+    //   ]
+    // }
   ],
   people: [
     {
